6ce2ed042c997732ed4d6267c900ca4db2584805
Evilweed <mcierpicki@gmail.com>
2022-03-01 01:51:20 +0100

    added title and description to fb share

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,9 @@ export default function _document() {
   return (
     <Html>
       <Head>
-        <meta property="og:image" content="https://ua-matching-needs.herokuapp.com/ukraine-flag.jpg"/>
+        <meta property="og:title" content="Wesprzyj Ukraińców!" />
+        <meta property="og:description" content="Wejdź na naszą platformę, zapoznaj się z potrzebami różnych instytucji i osób i wesprzyj nas w pomocy!" />
+        <meta property="og:image" content="http://ua-matching-needs.herokuapp.com/ukraine-flag.jpg"/>
         <meta property="og:image:secure_url" content="https://ua-matching-needs.herokuapp.com/ukraine-flag.jpg" />
       </Head>
       <body>
