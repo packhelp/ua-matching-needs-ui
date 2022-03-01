f934585196805893419d7325139947df9f61f31a
mastertoel <toel86@gmail.com>
2022-03-01 13:48:12 +0100

    introduce tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./**/**/Container.tsx",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
