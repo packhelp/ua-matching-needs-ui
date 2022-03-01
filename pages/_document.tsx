424ba286ecfd4653dde5bee827daad5bc5f848f0
mastertoel <toel86@gmail.com>
2022-03-01 09:47:32 +0100

    final fix of description

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,36 +4,7 @@ import { metaData } from "../src/utils/meta-data"
 export default function _document() {
   return (
     <Html>
-      <Head>
-        <title>{metaData.title}</title>
-        <meta
-          property="description"
-          content={metaData.description}
-          key="description"
-        />
-        <meta property="og:title" content={metaData.title} key="og-title" />
-        <meta
-          property="og:description"
-          content={metaData.description}
-          key="og-description"
-        />
-        <meta
-          property="og:image"
-          content="http://ua-matching-needs.herokuapp.com/ukraine-flag.jpg"
-          key="og-image"
-        />
-        <meta
-          property="og:image:secure_url"
-          content="https://ua-matching-needs.herokuapp.com/ukraine-flag.jpg"
-          key="og-image-secure"
-        />
-        <link
-          rel="icon"
-          href="http://ua-matching-needs.herokuapp.com/ukraine.svg"
-          type="image/svg+xml"
-          key="icon"
-        ></link>
-      </Head>
+      <Head></Head>
       <body>
         <Main />
         <NextScript />
--- src/components/layout/Container.tsx
+++ src/components/layout/Container.tsx
@@ -1,17 +1,46 @@
 import React, { FC } from "react"
 import { Box, Flex } from "@chakra-ui/react"
+import { metaData } from "../../utils/meta-data"
+import Head from "next/head"
 
 export const Container: FC = ({ children }) => {
   return (
-    <Flex justifyContent={"center"} alignItems={"center"}>
-      <Box
-        w="100%"
-        p={4}
-        borderWidth="1px"
-        borderRadius="lg"
-      >
-        {children}
-      </Box>
-    </Flex>
+    <>
+      <Head>
+        <title>{metaData.title}</title>
+        <meta
+          property="description"
+          content={metaData.description}
+          key="description"
+        />
+        <meta property="og:title" content={metaData.title} key="og-title" />
+        <meta
+          property="og:description"
+          content={metaData.description}
+          key="og-description"
+        />
+        <meta
+          property="og:image"
+          content="http://ua-matching-needs.herokuapp.com/ukraine-flag.jpg"
+          key="og-image"
+        />
+        <meta
+          property="og:image:secure_url"
+          content="https://ua-matching-needs.herokuapp.com/ukraine-flag.jpg"
+          key="og-image-secure"
+        />
+        <link
+          rel="icon"
+          href="http://ua-matching-needs.herokuapp.com/ukraine.svg"
+          type="image/svg+xml"
+          key="icon"
+        ></link>
+      </Head>
+      <Flex justifyContent={"center"} alignItems={"center"}>
+        <Box w="100%" p={4} borderWidth="1px" borderRadius="lg">
+          {children}
+        </Box>
+      </Flex>
+    </>
   )
 }
