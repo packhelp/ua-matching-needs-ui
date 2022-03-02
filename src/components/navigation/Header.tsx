7d972e5f8237426dd41a1c1e1f3652d3cc63739c
mastertoel <toel86@gmail.com>
2022-03-02 13:17:38 +0100

    add max header width

--- src/components/navigation/Header.tsx
+++ src/components/navigation/Header.tsx
@@ -8,7 +8,7 @@ export const Header = () => {
   return (
     <>
       <nav className="bg-white shadow sticky top-0">
-        <div className="mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
           <div className="flex justify-between h-16">
             <div className="flex">
               <MobileNavigation />
