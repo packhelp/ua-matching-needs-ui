11452486eaed3051937fee0ca34a8961f34e0754
mastertoel <toel86@gmail.com>
2022-03-02 12:46:16 +0100

    remove max width

--- src/components/navigation/Header.tsx
+++ src/components/navigation/Header.tsx
@@ -8,7 +8,7 @@ export const Header = () => {
   return (
     <>
       <nav className="bg-white shadow sticky top-0">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex justify-between h-16">
             <div className="flex">
               <MobileNavigation />
