b308b5c792a7cc54d7e5ece88aec569e7bee983a
Mariola Kasznicka <mariolakasznicka@Mariolas-MacBook-Pro.local>
2022-03-05 14:08:02 +0100

    Change green to blue for verified

--- pages/ticket/[id].tsx
+++ pages/ticket/[id].tsx
@@ -537,7 +537,7 @@ const TicketDetails: NextPage<{ ticket: TicketDetailsType }> = ({ ticket }) => {
                               xmlns="http://www.w3.org/2000/svg"
                               className="w-5 h-5 text-blue-400"
                               viewBox="0 0 20 20"
-                              fill="#61bd4f"
+                              fill="#4989bb"
                             >
                               <path
                                 fillRule="evenodd"
