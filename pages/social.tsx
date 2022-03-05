9f54efe3014773cab1893599ce32de4e6ea0b9a3
Evilweed <mcierpicki@gmail.com>
2022-03-06 00:20:30 +0100

    passing env to sentry

---
+++ pages/social.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next"
+import { Heading, Image } from "@chakra-ui/react"
+
+const SocialImage: NextPage = () => {
+
+  return <>
+    <Image src="/api/social-image/generate" />
+  </>
+}
+
+export default SocialImage
