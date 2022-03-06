b061200d66525f2374bb7c01e7db2cfdd5fedd85
Bartosz Stoiński <bartosz@Bartoszs-MBP-2.ht.home>
2022-03-06 11:15:49 +0100

    translations

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,4 +1,5 @@
 import React from "react"
+import { useTranslations } from "../hooks/translations"
 
 type ModalProps = {
   title: string
@@ -9,6 +10,7 @@ type ModalProps = {
 
 export const Modal = (props: ModalProps) => {
   const { title, content, actions, onClose } = props
+  const translations = useTranslations()
   return (
     <>
       <div
@@ -51,7 +53,7 @@ export const Modal = (props: ModalProps) => {
                 type="button"
                 className="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:ring-gray-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600"
               >
-                Close
+                {translations["generic"]["close"]}
               </button>
             </div>
           </div>
