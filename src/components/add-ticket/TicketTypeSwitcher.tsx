6b20590f8f81fb8b53bc2014ae6505e772ca81e4
Nick Olszanski <nick@dinosaurs-with-jetpacks.club>
2022-03-08 15:37:23 +0100

    pretty

--- src/components/add-ticket/TicketTypeSwitcher.tsx
+++ src/components/add-ticket/TicketTypeSwitcher.tsx
@@ -2,7 +2,7 @@ import { useTranslations } from "../../hooks/translations"
 
 export enum TicketType {
   Offer = "offer",
-  Need = "need"
+  Need = "need",
 }
 
 type TicketTypeSwitcherProps = {
@@ -21,14 +21,56 @@ export const TicketTypeSwitcher = (props: TicketTypeSwitcherProps) => {
     <div className="flex justify-between cursor-pointer">
       <div
         onClick={() => setType(TicketType.Need)}
-        className={`${isNeed ? 'border border-transparent bg-blue-500 shadow-sm hover:bg-blue-600 text-white' : 'border border-blue-500 text-black'} w-full relative inline-flex justify-center items-center ml-auto px-2 py-2 text-xs font-medium rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500`}
+        className={`${
+          isNeed
+            ? "border border-transparent bg-blue-500 shadow-sm hover:bg-blue-600 text-white"
+            : "border border-blue-500 text-black"
+        } w-full relative inline-flex justify-center items-center ml-auto px-2 py-2 text-xs font-medium rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500`}
       >
         <span>{translations["addTicket"]["iNeed"]}</span>
       </div>
-      <div className="w-16"/>
+      <div className="w-16" />
       <div
         onClick={() => setType(TicketType.Offer)}
-        className={`${isOffer ? 'border border-transparent bg-amber-300 shadow-sm hover:bg-amber-400 text-black' : 'border border-amber-300 text-black'} w-full relative inline-flex justify-center items-center ml-auto px-2 py-2 text-xs font-medium rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500`}
+        className={`${
+          isOffer
+            ? "border border-transparent bg-amber-300 shadow-sm hover:bg-amber-400 text-black"
+            : "border border-amber-300 text-black"
+        } w-full relative inline-flex justify-center items-center ml-auto px-2 py-2 text-xs font-medium rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500`}
+      >
+        <span>{translations["addTicket"]["iOffer"]}</span>
+      </div>
+    </div>
+  )
+}
+
+export const NeedTypeSwitcher = (props: TicketTypeSwitcherProps) => {
+  const { setType, selectedType } = props
+  const translations = useTranslations()
+
+  const isOffer = selectedType === TicketType.Offer
+  const isNeed = selectedType === TicketType.Need
+
+  return (
+    <div className="flex justify-between cursor-pointer">
+      <div
+        onClick={() => setType(TicketType.Need)}
+        className={`${
+          isNeed
+            ? "border border-transparent bg-blue-500 shadow-sm hover:bg-blue-600 text-white"
+            : "border border-blue-500 text-black"
+        } w-full relative inline-flex justify-center items-center ml-auto px-2 py-2 text-xs font-medium rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500`}
+      >
+        <span>{translations["addTicket"]["iNeed"]}</span>
+      </div>
+      <div className="w-16" />
+      <div
+        onClick={() => setType(TicketType.Offer)}
+        className={`${
+          isOffer
+            ? "border border-transparent bg-amber-300 shadow-sm hover:bg-amber-400 text-black"
+            : "border border-amber-300 text-black"
+        } w-full relative inline-flex justify-center items-center ml-auto px-2 py-2 text-xs font-medium rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500`}
       >
         <span>{translations["addTicket"]["iOffer"]}</span>
       </div>
