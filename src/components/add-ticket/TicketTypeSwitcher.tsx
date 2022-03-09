512261d938ded1de7e8a81861d42870f59612df5
antoniwrobel <31203524+antoniwrobel@users.noreply.github.com>
2022-03-09 22:31:25 +0100

    adjust add ticket menu styling

--- src/components/add-ticket/TicketTypeSwitcher.tsx
+++ src/components/add-ticket/TicketTypeSwitcher.tsx
@@ -9,13 +9,18 @@ type Option = {
 
 type TicketTypeSwitcherProps = {
   opts: Option[]
+  isColumn?: boolean
 }
 
 export const TicketTypeSwitcher = (props: TicketTypeSwitcherProps) => {
-  const { opts } = props
+  const { opts, isColumn } = props
 
   return (
-    <div className="flex justify-between cursor-pointer mb-4 text-center">
+    <div
+      className={`${
+        isColumn && "flex flex-col items-start"
+      } flex justify-between cursor-pointer mb-4 text-center`}
+    >
       {opts.map((option) => {
         return (
           <div
@@ -23,7 +28,9 @@ export const TicketTypeSwitcher = (props: TicketTypeSwitcherProps) => {
             onClick={option.onClick}
             className={`${
               option.active && "bg-blue-500 text-white"
-            } border-r-0 flex-col w-full relative inline-flex justify-center items-center ml-auto px-2 py-2 text-xs font-medium focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500`}
+            }  border-r-0 flex-col w-full relative inline-flex justify-center ml-auto px-2 py-2 text-xs font-medium focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 ${
+              isColumn ? "items-start " : "items-center"
+            }`}
           >
             {option.icon && <div className="mb-2">{option.icon}</div>}
             <span>{option.name}</span>
