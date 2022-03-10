b8dc6706da9f5f7f15506345dae47d4a391d3bc1
Nick Olszanski <nick@dinosaurs-with-jetpacks.club>
2022-03-11 00:18:18 +0100

    housing model ideas

---
+++ src/services/type.need.ts
@@ -0,0 +1,83 @@
+export type NeedHousingTypeFormData = {
+  // GENERIC START
+
+  /**
+   * Number of people
+   * Can kinda work for demand side
+   */
+  adults: number
+  children: number
+
+  /**
+   * contact
+   */
+  who: string
+  phone: string
+
+  alternative_contacts: string // new email, telegram etc.
+
+  /**
+   * Description
+   */
+
+  what: string
+  description: string
+
+  // GENERIC END
+
+  need_type: "housing"
+
+  /**
+   * Locations
+   * - Anywhere
+   * - exact cities
+   * - countries, regions, cities
+   *
+   * Exact:
+   * - exact city
+   * - exact country
+   *
+   * plaintext hard to pinpoint
+   * dropdown is long and hard to populate
+   *
+   */
+  where_tag: number // LocationTag ID - filter out not cit
+  housing_where_string: string // plain string harder to place on map
+
+  /**
+   * Housing times
+   * In case of places to sleep dates/time is the most important.
+   *
+   * I need it right now for about 10 days
+   * I need it from March 15th
+   * I need it for 2 night next week
+   *
+   * When?
+   * How long
+   *
+   * # Duration of stay
+   *
+   * could have simple options like:
+   * - 1 day
+   * - coupole of days
+   * - week or two
+   * - months
+   *
+   * or:
+   * - no idea
+   * - until the war ends
+   * - until find something different
+   */
+  housing_how_long_text?: string
+
+  /**
+   * Payments
+   *
+   * I am willing to pay
+   */
+
+  // Pets
+  housing_pets: boolean
+  housing_pets_description?: number
+  housing_pets_number?: number
+}
