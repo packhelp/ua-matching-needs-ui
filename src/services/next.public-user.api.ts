40b8ad021dcefd329e218fa76286d8941ba2a40d
Nick Olszanski <nick@dinosaurs-with-jetpacks.club>
2022-03-11 00:28:11 +0100

    wip model

---
+++ src/services/next.public-user.api.ts
@@ -0,0 +1,24 @@
+// const newTicketData = {
+//   what,
+//   description,
+//   expirationTimestampSane,
+//   phone,
+//   who,
+//   petsNumber: has_pets ? petsNumber : 0,
+//   adults: adults ? adults : 0,
+//   children: children ? children : 0,
+//   has_pets: !has_pets ? "0" : "1",
+
+//   // This is housing request, so hardcore a trip tag
+//   need_tag_id: [{ need_tag_id: { id: TagConstIds.housing } }],
+
+//   // tripe specific
+//   need_type: "housing",
+//   where_tag,
+//   housing_how_long_text,
+// }
+
+// console.log(newTicketData)
+// return axios.post(`/api/add-ticket`, newTicketData)
+
+export class NextPublicApi {}
