a1316ea01c8fea2365678b5a1f36d68d8a69159f
Sara Vieira <hey@iamsaravieira.com>
2022-03-13 19:20:38 +0100

    add merged things

--- src/components/ticket-list/single-ticket/HousingSection.tsx
+++ src/components/ticket-list/single-ticket/HousingSection.tsx
@@ -2,9 +2,12 @@ import { GoPerson } from "react-icons/go"
 import { FaClock, FaDog, FaMap } from "react-icons/fa"
 import { MdChildFriendly } from "react-icons/md"
 import { NeedHousing } from "../../../services/ticket.class"
+import { housingLabels } from "../../add-ticket/hooks/use-housing-options"
+import { useLocationTags } from "../../add-ticket/hooks/use-location-tags"
 
 export const HousingSection = ({ need }: { need: NeedHousing }) => {
   const { dtoHousing: housing } = need
+  const { mappedLocationTags } = useLocationTags()
 
   return (
     <div className="border-t border-b border-gray-200 bg-slate-50 px-4 py-5 p-1">
@@ -29,14 +32,21 @@ export const HousingSection = ({ need }: { need: NeedHousing }) => {
         )}
       </div>
       <div className="flex justify-around mt-2">
-        {housing.housing_when_leave_text && (
-          <div className="flex gap-2 items-center">
-            <FaClock />
-          </div>
-        )}
+        {housing.housing_when_leave_text &&
+          housingLabels[housing.housing_when_leave_text] && (
+            <div className="flex gap-2 items-center">
+              <FaClock />
+              {housingLabels[housing.housing_when_leave_text]}
+            </div>
+          )}
         {housing.housing_when_leave_text && (
           <div className="flex gap-2 items-center">
             <FaMap />
+            {
+              mappedLocationTags.find(
+                (loc) => loc.value === housing.housing_where_location_tag
+              )?.label
+            }
           </div>
         )}
       </div>
